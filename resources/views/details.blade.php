e3c3e5e5bf935c4864bb9a4e7403e8080c4bda92
mohamedslima <hammaslima@gmail.com>
2022-06-30 09:52:39 +0200

    completed exerc

---
+++ resources/views/details.blade.php
@@ -0,0 +1,11 @@
+<h1>
+    Ciao questa è la pagina dettagli
+</h1>
+<ul>
+    <li>
+        <a href="{{ route('home') }}">Home</a>
+    </li>
+    <li>
+        <a href="{{ route('details') }}">Details</a>
+    </li>
+</ul>
